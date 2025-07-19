dd40ee955b6ed480a98eaecd359e24068d668a6c
Robert Carr <racarrg@gmail.com>
2025-07-19 14:06:24 -0700

    Update api server defaults

--- hy3dshape/api_server.py
+++ hy3dshape/api_server.py
@@ -217,10 +217,10 @@ class ModelWorker:
             params['generator'] = torch.Generator(self.device).manual_seed(seed)
             params['octree_resolution'] = 256#params.get("octree_resolution", 256)
             params['num_inference_steps'] = 50#params.get("num_inference_steps", 50)
-            params['guidance_scale'] = 15#params.get('guidance_scale', 15.0)
+            params['guidance_scale'] = 10#params.get('guidance_scale', 15.0)
             params['mc_algo'] = 'mc'
             params['extra_cond_tok'] = self.cond_tok
-            params['append_extra_cond_tok'] = False
+            params['append_extra_cond_tok'] = True
             import time
             start_time = time.time()
             mesh = self.pipeline(**params)[0]
